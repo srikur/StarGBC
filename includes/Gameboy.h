ffcac862b52f4b09e7a5e0122bd8dda6f7e90cbf
Srikur Kanuparthy <srikur@users.noreply.github.com>
2025-10-05 13:22:01 -0700

    CGB Sound Fix (#31)
    * Move audio definitions to src file
    
    * Implement double speed for CGB
    
    * Remove tick variable in Channel3
    
    * Update readme and test data

--- includes/Gameboy.h
+++ includes/Gameboy.h
@@ -59,6 +59,7 @@ class Gameboy {
     uint8_t mCycleCounter{0x01};
     uint8_t tCycleCounter{0x00};
     uint32_t masterCycles{0x00000000};
+    uint32_t cyclesPerFrame{0x00000000};
     uint8_t interruptBit{0x00};
     uint8_t interruptMask{0x00};
     uint16_t pc = 0x00;
@@ -102,7 +103,9 @@ public:
         } else if ((bus->cartridge_->ReadByte(0x143) & 0x80) == 0x80) {
             mode_ = Mode::CGB_GBC;
             bus->gpu_->hardware = GPU::Hardware::CGB;
+            bus->audio_->SetDMG(false);
         }
+        cyclesPerFrame = bus->gpu_->hardware == GPU::Hardware::DMG ? DMG_CYCLES_PER_SECOND : CGB_CYCLES_PER_SECOND;
         if (!bios_path_.empty()) {
             bus->bootromRunning = true;
             InitializeBootrom();
