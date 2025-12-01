48c74dbf00e78320a7676b2e55e99eabe374121d
srikur <srikur@users.noreply.github.com>
2025-11-30 19:10:10 -0500

    Partial refactor of CPU out of Gameboy

--- src/Gameboy.cpp
+++ src/Gameboy.cpp
@@ -17,41 +17,23 @@ bool Gameboy::ShouldRender() const {
 }
 
 void Gameboy::Save() const {
-    bus->cartridge_->Save();
+    cpu->Save();
 }
 
 void Gameboy::KeyUp(const Keys key) const {
-    bus->KeyUp(key);
+    cpu->KeyUp(key);
 }
 
 void Gameboy::KeyDown(const Keys key) const {
-    bus->KeyDown(key);
+    cpu->KeyDown(key);
 }
 
 uint32_t *Gameboy::GetScreenData() const {
-    return bus->gpu_->screenData.data();
-}
-
-uint8_t Gameboy::DecodeInstruction(const uint8_t opcode, const bool prefixed) {
-    // std::fprintf(stderr, "Running instruction %02X %s with mCycleCounter = %d, masterCycles = %d\n", opcode, prefixed ? "(prefixed)" : "", mCycleCounter, masterCycles);
-    return prefixed
-               ? instructions->prefixedInstr(opcode, *this)
-               : instructions->nonPrefixedInstr(opcode, *this);
+    return cpu->GetScreenData();
 }
 
 void Gameboy::InitializeBootrom() const {
-    std::ifstream file(bios_path_, std::ios::binary);
-    file.unsetf(std::ios::skipws);
-
-    file.seekg(0, std::ios::end);
-    const std::streampos fileSize = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    bus->bootrom.reserve(fileSize);
-    bus->bootrom.insert(bus->bootrom.begin(),
-                        std::istream_iterator<uint8_t>(file),
-                        std::istream_iterator<uint8_t>());
-    file.close();
+    cpu->InitializeBootrom(bios_path_);
 }
 
 uint32_t Gameboy::RunHDMA() const {
@@ -124,7 +106,7 @@ void Gameboy::InitializeSystem() {
 }
 
 void Gameboy::ToggleSpeed() {
-    speedMultiplier = (speedMultiplier == 1) ? 4 : 1;
+    speedMultiplier = speedMultiplier == 1 ? 4 : 1;
 }
 
 void Gameboy::SetThrottle(const bool throttle) {
@@ -141,7 +123,6 @@ void Gameboy::AdvanceFrame() {
     if (masterCycles % speedDivider == 0) bus->UpdateSerial();
     if (masterCycles % speedDivider == 0) bus->UpdateDMA();
     if (masterCycles % GRAPHICS_CLOCK_DIVIDER == 0) bus->UpdateGraphics();
-    // if (interruptState == InterruptState::M1 && instrComplete && tCycleCounter == 0) PrintCurrentValues();
     masterCycles++;
     // need to add hdma tick
 }
@@ -193,38 +174,6 @@ void Gameboy::UpdateEmulator() {
     const auto elapsed = clock::now() - frameStart;
     if (const auto effectiveFrameTime = kFramePeriod / speedMultiplier; throttleSpeed && elapsed < effectiveFrameTime)
         std::this_thread::sleep_for(effectiveFrameTime - elapsed);
-}
-
-void Gameboy::PrintCurrentValues() {
-    if ((previousInstruction & 0xFF00) >> 8 == 0xCB) return;
-    if (previousPrefixed) previousInstruction = 0xCB00 | (previousInstruction & 0x00FF);
-    const std::string text = std::format(
-        "----------------------------------------------\n"
-        "PC: {:#06x}  SP: {:#06x}  OP: {:#04x} — {}\n"
-        "A:{:02X}  F: Z{} N{} H{} C{}\n"
-        "B:{:02X}  C:{:02X}\n"
-        "D:{:02X}  E:{:02X}\n"
-        "H:{:02X}  L:{:02X}\n\n"
-        "LCDC:{:02X} STAT:{:02X} LY:{:02X} CNT:{:02X}\n"
-        "IE:{:02X} IF:{:02X} IME:{:02X}\n"
-        "DIV:{:02X} TIMA:{:02X} TMA:{:02X} TAC:{:02X}"
-        "\n----------------------------------------------\n",
-        previousPC, sp, previousInstruction, instructions->GetMnemonic(previousPrefixed ? 0xCB00 | previousInstruction : previousInstruction),
-        regs->a,
-        regs->FlagZero() ? 1 : 0, regs->FlagSubtract() ? 1 : 0,
-        regs->FlagHalf() ? 1 : 0, regs->FlagCarry() ? 1 : 0,
-        regs->b, regs->c, regs->d, regs->e,
-        regs->h, regs->l,
-        bus->gpu_->lcdc, bus->gpu_->stat.value(), bus->gpu_->currentLine, bus->gpu_->scanlineCounter,
-        bus->interruptEnable, bus->interruptFlag, bus->interruptMasterEnable ? 1 : 0,
-        bus->timer_.divCounter, bus->timer_.tima, bus->timer_.tma, bus->timer_.tac);
-    // const std::string text = std::format("{:#04x} - {}\n", currentInstruction, instructions->GetMnemonic(prefixed ? 0xCB00 | currentInstruction : currentInstruction));
-    // print to file 'debug.txt' -- append
-    // std::printf("%s", text.c_str());
-    // std::fflush(stdout);
-    // std::ofstream file("debug.txt", std::ios_base::app);
-    // file << text;
-    // file.close();
 }
 
 inline uint8_t interruptAddress(const uint8_t bit) {
@@ -316,59 +265,4 @@ bool Gameboy::ProcessInterrupts() {
         }
     }
     return true;
-}
-
-void Gameboy::SaveState(int slot) const {
-    if (pc < 0x100) {
-        std::printf("Error: Cannot save state while bootrom is running\n");
-        return;
-    }
-
-    bus->cartridge_->Save();
-
-    std::ofstream stateFile(std::format("{}.sgm{}", Cartridge::RemoveExtension(rom_path_), slot), std::ios::binary);
-    if (!stateFile.is_open()) {
-        std::printf("Error: Could not open state file for saving\n");
-        return;
-    }
-
-    stateFile.write(reinterpret_cast<const char *>(&pc), sizeof(pc));
-    stateFile.write(reinterpret_cast<const char *>(&sp), sizeof(sp));
-    stateFile.write(reinterpret_cast<const char *>(&icount), sizeof(icount));
-    stateFile.write(reinterpret_cast<const char *>(&halted), sizeof(halted));
-    stateFile.write(reinterpret_cast<const char *>(&haltBug), sizeof(haltBug));
-    stateFile.write(reinterpret_cast<const char *>(&currentInstruction), sizeof(currentInstruction));
-
-    regs->SaveState(stateFile);
-    bus->SaveState(stateFile);
-
-    stateFile.close();
-}
-
-void Gameboy::LoadState(int slot) {
-    if (pc < 0x100) {
-        std::printf("Error: Cannot load state while bootrom is running\n");
-        return;
-    }
-
-    bus->cartridge_->Save();
-    paused = true;
-    std::ifstream stateFile(std::format("{}.sgm{}", Cartridge::RemoveExtension(rom_path_), slot), std::ios::binary);
-    if (!stateFile.is_open()) {
-        std::printf("Error: Could not open state file for loading\n");
-        return;
-    }
-
-    stateFile.read(reinterpret_cast<char *>(&pc), sizeof(pc));
-    stateFile.read(reinterpret_cast<char *>(&sp), sizeof(sp));
-    stateFile.read(reinterpret_cast<char *>(&icount), sizeof(icount));
-    stateFile.read(reinterpret_cast<char *>(&halted), sizeof(halted));
-    stateFile.read(reinterpret_cast<char *>(&haltBug), sizeof(haltBug));
-    stateFile.read(reinterpret_cast<char *>(&currentInstruction), sizeof(currentInstruction));
-
-    regs->LoadState(stateFile);
-    bus->LoadState(stateFile);
-
-    stateFile.close();
-    paused = false;
 }
