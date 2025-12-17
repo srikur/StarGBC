7d019f99a41bd82f3df57dc98dbe45b09279b48a
srikur <srikur@users.noreply.github.com>
2025-12-16 22:47:02 -0800

    MockCPU to build instruction tests

--- tests/InstructionTests.h
+++ tests/InstructionTests.h
@@ -1,11 +1,39 @@
+#ifndef STARGBC_INSTRUCTIONTESTS_H
+#define STARGBC_INSTRUCTIONTESTS_H
+
 #include <Gameboy.h>
 #include "doctest.h"
 #include "mocks/MockBus.h"
+#include "mocks/MockCPU.h"
 
-int ExecuteInstructionTests() {
+class InstructionTests {
+public:
+
+    /**
+     * Functions:
+     * - LD rr, nn: Load 16-bit immediate into register pair
+     *      1. 0x01 - LD BC, n16
+     *
+     * - LN (nn), SP: Load SP into given address
+     * - LD, SP, HL: Load SP from HL
+     * - PUSH rr: Push register pair onto stack
+     * - POP rr: Pop register pair from stack
+     * - LD HL, SP+e: Load SP plus signed immediate into HL
+     */
+    void Load16Bit() {
+        // instructions
+    }
+
+    static void ExecuteAll() {
+        InstructionTests tests{};
+        tests.Load16Bit();
+    }
+private:
     Registers regs{};
     Interrupts interrupts{};
     MockBus bus{};
-    Instructions instructions{regs, bus, interrupts};
-    return 0;
-}
+    MockCPU<MockBus> cpu{bus};
+    Instructions<MockCPU<MockBus>> instructions{regs, interrupts};
+};
+
+#endif //STARGBC_INSTRUCTIONTESTS_H
--- tests/main.cpp
+++ tests/main.cpp
@@ -3,5 +3,20 @@
 #include "TestRoms.h"
 
 int main(const int argc, char **argv) {
-    ExecuteTestRoms(argc, argv);
+    const std::string_view arg = argc > 1 ? argv[1] : "";
+    if (arg == "--instructions") {
+        InstructionTests::ExecuteAll();
+    } else if (arg == "--blargg") {
+        ExecuteTestRoms(argc, argv);
+    } else if (arg == "--all") {
+        InstructionTests::ExecuteAll();
+        ExecuteTestRoms(argc, argv);
+    } else {
+        std::fprintf(stderr, "USAGE: StarGBC_Tests [options]\n"
+                     "Options:\n"
+                     "  --instructions      instruction unit tests\n"
+                     "  --blargg            blargg test roms\n"
+                     "  --all              all tests\n");
+        return -1;
+    }
 }
