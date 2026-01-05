890700c6d482f066655d9709702e18651f2c5195
srikur <srikur@users.noreply.github.com>
2026-01-05 10:39:37 -0500

    HDMA cycle granularity behavior

--- includes/HDMA.h
+++ includes/HDMA.h
@@ -7,12 +7,19 @@ enum class HDMAMode {
     GDMA, HDMA
 };
 
+enum class HDMAStep {
+    Read, Write
+};
+
 struct HDMA {
     uint16_t hdmaSource{};
     uint16_t hdmaDestination{};
     uint8_t hdmaRemain{};
     uint8_t hdmaStartDelay{};
     uint8_t hdma5{};
+    uint8_t bytesThisBlock{};
+    uint8_t byte{};
+    HDMAStep step{HDMAStep::Read};
     HDMAMode hdmaMode{HDMAMode::GDMA};
     bool hdmaActive{};
 
