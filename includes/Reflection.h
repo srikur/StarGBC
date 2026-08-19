d75475292967016d3e6b0a272c999b1a282806b1
srikur <srikur@users.noreply.github.com>
2026-08-18 19:39:23 -0500

    Reflection function improvements

--- includes/Reflection.h
+++ includes/Reflection.h
@@ -16,18 +16,30 @@ consteval bool IsStateAware(const std::meta::info m) {
 }
 
 consteval bool IsNotReferenceType(const std::meta::info m) {
-    return !std::meta::is_reference_type(m);
+    return !std::meta::is_reference_type(std::meta::type_of(m));
+}
+
+consteval bool IsSerializableLeaf(const std::meta::info type) {
+    return std::meta::has_unique_object_representations(type)
+           || std::meta::is_floating_point_type(type);
 }
 
 consteval std::vector<std::meta::info> StateMembersOf(const std::meta::info type) {
-    return std::meta::nonstatic_data_members_of(type, std::meta::access_context::unchecked()) |
-           std::views::filter(IsStateAware) | std::views::filter(IsNotReferenceType) | std::ranges::to<std::vector>();
+    if (!std::meta::is_class_type(type) && !std::meta::is_union_type(type))
+        return {};
+    return std::meta::nonstatic_data_members_of(type, std::meta::access_context::unchecked())
+           | std::views::filter(IsStateAware)
+           | std::views::filter(IsNotReferenceType)
+           | std::ranges::to<std::vector>();
 }
 
 consteval std::size_t StateSizeOf(const std::meta::info type) {
+    if (std::meta::is_array_type(type)) {
+        return std::meta::extent(type) * StateSizeOf(std::meta::remove_extent(type));
+    }
     const std::vector<std::meta::info> members = StateMembersOf(type);
     if (members.empty()) {
-        if (!std::meta::has_unique_object_representations(type)) {
+        if (!IsSerializableLeaf(type)) {
             throw std::invalid_argument("leaf state type has padding or is not trivially copyable");
         }
         return std::meta::size_of(type);
